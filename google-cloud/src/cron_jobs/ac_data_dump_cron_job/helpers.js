b5df9583fde9c99170a27c84d7d10eb772543770
Nick Bucher <behappy54321@gmail.com>
2021-03-09 19:01:54 -0500

    fixed the cron job script so that the bulkload wont break if the column names are changed by active campaign api updates.

--- google-cloud/src/cron_jobs/ac_data_dump_cron_job/helpers.js
+++ google-cloud/src/cron_jobs/ac_data_dump_cron_job/helpers.js
@@ -1,30 +1,30 @@
 exports.sleep = (ms) => {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-};
+    setTimeout(resolve, ms)
+  })
+}
 
 exports.bulkload = async (tableName, data2D, conn) => {
   // Check that data2D is a 2D array of objects.
-  if (data2D.length === 0 || Object.keys(data2D[0]).length === 0) return false;
+  if (data2D.length === 0 || Object.keys(data2D[0]).length === 0) return false
   // Bulkload into tableName, replacing on duplicate keys.
   // REPLACE works exactly like INSERT, except that if an old row in the table has the same value as a new row for a PRIMARY KEY or a UNIQUE index, the old row is deleted before the new row is inserted.
-  var cmd = "REPLACE INTO " + tableName + " (";
+  var cmd = 'REPLACE INTO ' + tableName + ' ('
   cmd +=
     Object.keys(data2D[0])
-      .map((k) => "`" + k + "`")
-      .join(", ") + ") VALUES ";
+      .map((k) => '`' + k + '`')
+      .join(', ') + ') VALUES '
   // Set up the query to be parameterized.
-  const params = {}; //[];
+  const params = {} //[];
   for (var i = 0; i < data2D.length; i++) {
     // Append the correct number of parameter placeholders to the query.
     cmd +=
-      "(" +
+      '(' +
       Object.values(data2D[i])
         // .map((v) => "?")
-        .map((v, idx) => ":param_" + i + "_" + idx)
-        .join(", ") +
-      "), ";
+        .map((v, idx) => ':param_' + i + '_' + idx)
+        .join(', ') +
+      '), '
     // Push the values on to the params object.
     // if (data2D[i].subject !== undefined) console.log("Inserting subject line: " + data2D[i].subject);
     // params.push(
@@ -34,17 +34,17 @@ exports.bulkload = async (tableName, data2D, conn) => {
     //   })
     // );
     Object.values(data2D[i]).forEach((v, idx) => {
-      var val = "";
-      if (typeof v === "string") val = v;
-      else val = JSON.stringify(v);
-      params["param_" + i + "_" + idx] = val;
-    });
+      var val = ''
+      if (typeof v === 'string') val = v
+      else val = JSON.stringify(v)
+      params['param_' + i + '_' + idx] = val
+    })
   }
   // console.log("Updating table: " + tableName);
-  cmd = cmd.substr(0, cmd.length - 2) + ";";
+  cmd = cmd.substr(0, cmd.length - 2) + ';'
   // console.log(cmd);
-  await conn.query({ sql: cmd, params: params });
-  console.log("Table updated:", tableName);
+  await conn.query({ sql: cmd, params: params })
+  console.log('Table updated:', tableName)
   // conn.query(cmd, params, function (error, results) {
   //   if (error) {
   //     console.log(error);
@@ -53,4 +53,215 @@ exports.bulkload = async (tableName, data2D, conn) => {
   //   console.log("Table updated: " + tableName);
   // });
   // console.log(cmd + "\n\n\n");
-};
+}
+
+// ----------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------- //
+// ------- FORMATS DATA FOR TABLE (to prevent activeCampaign API changes from breaking this job) ------- //
+const assignKeys = (d, keys) => {
+  var newObj = {}
+  var dKeys = Object.keys(d)
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i]
+    var value = ''
+    if (dKeys.find((dk) => dk === key) !== undefined) value = d[key]
+    newObj[key] = value
+  }
+  return newObj
+}
+exports.formatDataForTable = (data, tableName) => {
+  var tableColumns = []
+  switch (tableName) {
+    case '`sendoptimizer`.`ac_campaignmessages`':
+      tableColumns = [
+        'id',
+        'messageid',
+        'campaignid',
+        'percentage',
+        'sourcesize',
+        'send_amt',
+        'total_amt',
+        'opens',
+        'uniqueopens',
+        'linkclicks',
+        'uniquelinkclicks',
+        'subscriberclicks',
+        'forwards',
+        'uniqueforwards',
+        'hardbounces',
+        'softbounces',
+        'unsubscribes',
+        'unsubreasons',
+        'updates',
+        'socialshares',
+        'replies',
+        'uniquereplies',
+        'spamcheck_score',
+        'spamcheck_max',
+        'initial_split_percentage',
+        'screenshot',
+        'subject',
+        'message',
+        'links',
+        'campaign',
+      ]
+      break
+    case '`sendoptimizer`.`ac_campaigns`':
+      tableColumns = [
+        'id',
+        'type',
+        'userid',
+        'segmentid',
+        'bounceid',
+        'realcid',
+        'sendid',
+        'threadid',
+        'seriesid',
+        'formid',
+        'basetemplateid',
+        'basemessageid',
+        'addressid',
+        'source',
+        'name',
+        'cdate',
+        'mdate',
+        'sdate',
+        'ldate',
+        'send_amt',
+        'total_amt',
+        'opens',
+        'uniqueopens',
+        'linkclicks',
+        'uniquelinkclicks',
+        'subscriberclicks',
+        'forwards',
+        'uniqueforwards',
+        'hardbounces',
+        'softbounces',
+        'unsubscribes',
+        'unsubreasons',
+        'updates',
+        'socialshares',
+        'replies',
+        'uniquereplies',
+        'status',
+        'public',
+        'mail_transfer',
+        'mail_send',
+        'mail_cleanup',
+        'mailer_log_file',
+        'tracklinks',
+        'tracklinksanalytics',
+        'trackreads',
+        'trackreadsanalytics',
+        'analytics_campaign_name',
+        'tweet',
+        'facebook',
+        'survey',
+        'embed_images',
+        'htmlunsub',
+        'textunsub',
+        'htmlunsubdata',
+        'textunsubdata',
+        'recurring',
+        'willrecur',
+        'split_type',
+        'split_content',
+        'split_offset',
+        'split_offset_type',
+        'split_winner_messageid',
+        'split_winner_awaiting',
+        'responder_offset',
+        'responder_type',
+        'responder_existing',
+        'reminder_field',
+        'reminder_format',
+        'reminder_type',
+        'reminder_offset',
+        'reminder_offset_type',
+        'reminder_offset_sign',
+        'reminder_last_cron_run',
+        'activerss_interval',
+        'activerss_url',
+        'activerss_items',
+        'ip4',
+        'laststep',
+        'managetext',
+        'schedule',
+        'scheduleddate',
+        'waitpreview',
+        'deletestamp',
+        'replysys',
+        'created_timestamp',
+        'updated_timestamp',
+        'created_by',
+        'updated_by',
+        'ip',
+        // 'series_send_lock_time', <- this column was added by Active Campaign, and was breaking this job.
+        'segmentname',
+        'has_predictive_content',
+        'screenshot',
+        'campaignMessages',
+        'links',
+        'user',
+        'automation',
+      ]
+      break
+    case '`sendoptimizer`.`ac_links`':
+      tableColumns = [
+        'id',
+        'campaignid',
+        'messageid',
+        'link',
+        'name',
+        'ref',
+        'tracked',
+        'created_timestamp',
+        'updated_timestamp',
+        'created_by',
+        'updated_by',
+        'uniquelinkclicks',
+        'linkclicks',
+        'links',
+        'campaign',
+        'message',
+      ]
+      break
+    case '`sendoptimizer`.`ac_messages`':
+      tableColumns = [
+        'id',
+        'userid',
+        'ed_instanceid',
+        'ed_version',
+        'cdate',
+        'mdate',
+        'name',
+        'fromname',
+        'fromemail',
+        'reply2',
+        'priority',
+        'charset',
+        'encoding',
+        'format',
+        'subject',
+        'preheader_text',
+        'text',
+        'html',
+        'htmlfetch',
+        'textfetch',
+        'hidden',
+        'preview_mime',
+        'preview_data',
+        'has_predictive_content',
+        'links',
+        'user',
+      ]
+      break
+    default:
+      console.log('Invalid table name', tableName, data)
+      return
+  }
+  for (var i = 0; i < data.length; i++)
+    data[i] = assignKeys(data[i], tableColumns)
+}
--- google-cloud/src/cron_jobs/ac_data_dump_cron_job/index.js
+++ google-cloud/src/cron_jobs/ac_data_dump_cron_job/index.js
@@ -1,6 +1,6 @@
-var axios = require("axios");
-var { MySQL } = require("mysql-promisify");
-var { bulkload, sleep } = require("./helpers");
+var axios = require('axios')
+var { MySQL } = require('mysql-promisify')
+var { bulkload, sleep, formatDataForTable } = require('./helpers')
 
 /**
  * Triggered from a message on a Cloud Pub/Sub topic.
@@ -19,9 +19,9 @@ exports.main = (event, context) => {
     AC_API_TOKEN,
     CLOUD_SQL_CONNECTION_NAME,
     DB_SOCKET_PATH,
-  } = process.env;
-  const dbSocketPath = DB_SOCKET_PATH || "/cloudsql";
-  const isProd = process.env.NODE_ENV === "production";
+  } = process.env
+  const dbSocketPath = DB_SOCKET_PATH || '/cloudsql'
+  const isProd = process.env.NODE_ENV === 'production'
 
   // --- CLOUD SQL CONNECTION --- //
   var connection = new MySQL({
@@ -29,99 +29,100 @@ exports.main = (event, context) => {
     user: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME,
-    charset: "utf8mb4",
+    charset: 'utf8mb4',
     timeout: 60000,
     // Production Mode
     ...(isProd && {
       socketPath: `${dbSocketPath}/${CLOUD_SQL_CONNECTION_NAME}`,
     }),
-  });
+  })
 
-  const base = "https://" + AC_ACCOUNT + ".api-us1.com/api/3/";
-  const pageSize = 10;
+  const base = 'https://' + AC_ACCOUNT + '.api-us1.com/api/3/'
+  const pageSize = 10
 
   async function doDataDump(timeframeHoursToUpdate = -1) {
     // Do initial data check (to get totalRows and numPages).
     const initialRes = await axios.get(
       base +
-        "campaigns?orders[sdate]=DESC&filters[type]=split&limit=1&offset=0",
-      { headers: { "Api-Token": AC_API_TOKEN } }
-    );
-    var totalRows = Number.parseInt(initialRes.data.meta.total);
-    var numPages = Math.ceil(totalRows / pageSize);
+        'campaigns?orders[sdate]=DESC&filters[type]=split&limit=1&offset=0',
+      { headers: { 'Api-Token': AC_API_TOKEN } },
+    )
+    var totalRows = Number.parseInt(initialRes.data.meta.total)
+    var numPages = Math.ceil(totalRows / pageSize)
     console.log(
-      "---- META ----",
-      "\nNumber of rows: " + totalRows,
-      "\nPage size: " + pageSize,
-      "\nNumber of pages: " + numPages
-    );
+      '---- META ----',
+      '\nNumber of rows: ' + totalRows,
+      '\nPage size: ' + pageSize,
+      '\nNumber of pages: ' + numPages,
+    )
     // For each page:
     for (let i = 0; i < numPages; i++) {
-      var pageOffset = pageSize * i;
+      var pageOffset = pageSize * i
       // Do Active Campaign API request.
-      const options = "&limit=" + pageSize + "&offset=" + pageOffset;
+      const options = '&limit=' + pageSize + '&offset=' + pageOffset
       var pageRes = await axios.get(
         base +
-          "campaigns?include=campaignMessages,campaignMessages.message,links&orders[sdate]=DESC&filters[type]=split" +
+          'campaigns?include=campaignMessages,campaignMessages.message,links&orders[sdate]=DESC&filters[type]=split' +
           options,
-        { headers: { "Api-Token": AC_API_TOKEN } }
-      );
-      console.log("\nGot page " + i + " / " + numPages, pageRes.data);
-      var dataToDump = JSON.parse(JSON.stringify(pageRes.data));
-      delete dataToDump.meta;
+        { headers: { 'Api-Token': AC_API_TOKEN } },
+      )
+      console.log('\nGot page ' + i + ' / ' + numPages, pageRes.data)
+      var dataToDump = JSON.parse(JSON.stringify(pageRes.data))
+      delete dataToDump.meta
       // Check if we need to update all of these campaigns, or if we should stop after this page.
-      var shouldStopAfterThisPage = false;
+      var shouldStopAfterThisPage = false
       if (timeframeHoursToUpdate !== -1) {
         // Check each campaign to only update the ones within the timeframeHoursToUpdate.
-        var now = new Date();
+        var now = new Date()
         for (let j = 0; j < pageRes.data.campaigns.length; j++) {
-          var c = pageRes.data.campaigns[j];
-          var sdate = new Date(c.sdate);
-          var hoursSinceSend = (now - sdate) / (60 * 60 * 1000);
+          var c = pageRes.data.campaigns[j]
+          var sdate = new Date(c.sdate)
+          var hoursSinceSend = (now - sdate) / (60 * 60 * 1000)
           // If this is outside of the campaign timeframe to update.
           if (hoursSinceSend > timeframeHoursToUpdate) {
             // Stop making page requests after this page is put into the database.
-            shouldStopAfterThisPage = true;
+            shouldStopAfterThisPage = true
             // Remove associated data for this campaign, campaignMessages, messages, and links.
             dataToDump.campaigns = dataToDump.campaigns.filter(
-              (c2) => c2.id !== c.id
-            );
+              (c2) => c2.id !== c.id,
+            )
             dataToDump.campaignMessages = dataToDump.campaignMessages.filter(
               (cm) => {
                 if (cm.campaignid === c.id) {
                   // If this campaignMessage is to be removed.
                   // Remove the associated message.
                   dataToDump.messages = dataToDump.messages.filter(
-                    (m) => m.id !== cm.messageid
-                  );
+                    (m) => m.id !== cm.messageid,
+                  )
                   // Filter this campaignMessage out of the results.
-                  return false;
-                } else return true;
-              }
-            );
+                  return false
+                } else return true
+              },
+            )
             dataToDump.links = dataToDump.links.filter(
-              (l) => l.campaignid !== c.id
-            );
+              (l) => l.campaignid !== c.id,
+            )
           }
         }
       }
       // Bulkload into the correct tables.
       // For each key, k (campaignMessages, messages, links, campaigns, meta):
-      var dataToDumpKeys = Object.keys(dataToDump);
+      var dataToDumpKeys = Object.keys(dataToDump)
       for (let dk = 0; dk < dataToDumpKeys.length; dk++) {
-        var k = dataToDumpKeys[dk];
+        var k = dataToDumpKeys[dk]
         // Bulkload (insert and update) the 2D array of of results for k into the associated database table.
-        var tableName = "`" + DB_NAME + "`.`ac_" + k.toLowerCase() + "`";
-        var data2D = dataToDump[k];
+        var tableName = '`' + DB_NAME + '`.`ac_' + k.toLowerCase() + '`'
+        var data2D = dataToDump[k]
+        formatDataForTable(data2D, tableName)
         // Wait for the bulkload to finish.
-        await bulkload(tableName, data2D, connection);
+        await bulkload(tableName, data2D, connection)
       }
-      if (shouldStopAfterThisPage) break;
+      if (shouldStopAfterThisPage) break
       // Wait between requests to prevent ActiveCampaign from capping/throttling us.
-      else await sleep(1000);
+      else await sleep(1000)
     }
-    console.log("\n------ Complete! ------");
-    connection.end();
+    console.log('\n------ Complete! ------')
+    connection.end()
   }
 
   /*
@@ -143,8 +144,8 @@ exports.main = (event, context) => {
 
   // Parameter is the number of hours before now to check and update.
   // Leaving it out will do the data dump for the entire database (which takes a long time)!!!
-  doDataDump(48);
-  // doDataDump();
-};
+  doDataDump(48)
+  // doDataDump()
+}
 
-if (process.env.NODE_ENV === "development") exports.main();
+if (process.env.NODE_ENV === 'development') exports.main()
